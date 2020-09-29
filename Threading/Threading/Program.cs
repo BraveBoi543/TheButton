7d85d8890b25383760d461e5e943fafa1abc1665
Jakal-lantern <57189817+Jakal-lantern@users.noreply.github.com>
2020-09-29 10:54:38 -0400

    Part 1 and 2 should be done
    All basic mechanics are done

--- Threading/Threading/Program.cs
+++ Threading/Threading/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.IO;
 
 namespace Threading
 {
@@ -15,8 +16,14 @@ namespace Threading
         public static class Globals
         {
             public static bool gameLoop = true;
+            public static bool hasHit = false;
+
             public static int maxTime = 10000;
             public static int currentTime = 0;
+
+            public static string folderPath = @"C:/Users/Public/Documents/";
+            public static string txtPath = @"C:/Users/Public/Documents/myData.txt";
+            public static string userName;
         }
 
 
@@ -27,12 +34,14 @@ namespace Threading
 
             int pointX = rand.Next(1920);
             int pointY = rand.Next(1080);
-            
 
+            Console.Write("Enter name: ");
+            Globals.userName = Console.ReadLine();
 
             // Game loop
             while (Globals.gameLoop)
             {
+                // Tick timer
                 Timer();
 
                 // Gather current cursor x and y values
@@ -46,16 +55,34 @@ namespace Threading
                 // Print name on single line
                 Console.Write($"\r{displayName}, {Globals.currentTime}");
                 
+                // Detect hit and exit loop
                 if (displayName == "Hit     ")
                 {
                     Globals.gameLoop = false;
+                    Globals.hasHit = true;
                 }
 
             }
 
-            Console.WriteLine("Game over");
+            // Output 
+            Console.WriteLine("\nGame over");
             Console.WriteLine($"You took {ConvertToSeconds(Globals.currentTime)} seconds");
+            Console.WriteLine("Press ENTER to exit. . .");
             Console.ReadKey();
+            // Record score on text file
+            WriteToTxt();
+
+            // Open 
+            if (Globals.hasHit)
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=mnpjpdhUNjY");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ ");
+                Console.WriteLine("You couldn't hit the borad side of a barn with a bowling ball. Maybe try again.");
+                Console.ReadKey();
+            }
             
         }
 
@@ -117,10 +144,53 @@ namespace Threading
             Thread.Sleep(10);
         }
 
+        // Calculate readable time
         public static int ConvertToSeconds(int totalTime)
         {
             int totalSeconds = totalTime / 100;
             return totalSeconds;
+        }
+
+        // Write score to text file
+        public static void WriteToTxt()
+        {
+            // Creates folder directory
+            System.IO.Directory.CreateDirectory(Globals.folderPath);
+
+            try
+            {
+                // Tests to see if file exits
+                // If not, creates file
+                if (!File.Exists(Globals.txtPath))
+                {
+                    using (StreamWriter sw = File.CreateText(Globals.txtPath)) ;
+                }
+                else
+                {
+                    using (StreamReader sr = File.OpenText(Globals.txtPath))
+                    {
+                        //OUTPUT TEXT IN FILE
+                        sr.Close();
+
+                        // 'true' modifier at end appends the text file
+                        StreamWriter sw = new StreamWriter(Globals.txtPath, true);
+                        sw.WriteLine($"{Globals.userName} took {ConvertToSeconds(Globals.currentTime)} second(s)");
+                        sw.Close();
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file was not found: {e}");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"The directory was not found: {e}");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The file could not be opened: {e}");
+            }
         }
     }
 }
